35a1da53cb04f82c874ec7d21a396ac1859605a0
AkshayShastrakar <akshay.shastrakar1@gmail.com>
2020-02-01 15:53:06 +0530

    SA-Import removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'src/environments/environment';
 import { AffliationComponent } from './affliation/affliation.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     AppComponent,
