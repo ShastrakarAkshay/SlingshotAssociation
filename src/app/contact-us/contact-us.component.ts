6033fcf26281bdbf41dbc861073d194ef873c32d
Shastrakar <ashastra@bmc.com>
2020-02-06 18:36:33 +0530

    SA-contact us page updated

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  users: any[] = [1,2,3,4];
+  users: any[] = [1,2,3];
   constructor() { }
 
   ngOnInit() {
