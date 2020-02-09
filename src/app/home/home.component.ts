0a6060860a441dc93f7e3d658af27184c3a3ba6f
AkshayShastrakar <akshay.shastrakar1@gmail.com>
2020-02-09 23:51:41 +0530

    SA-home page updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   list:any [] = [1,2,3,4];
+  members:any [] = [1,2,3,4];
+
   constructor() { }
 
   ngOnInit() {
