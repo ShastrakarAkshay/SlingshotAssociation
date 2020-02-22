2090650035bec610bfad763c35b7665b406b2751
AkshayShastrakar <akshay.shastrakar1@gmail.com>
2020-02-22 11:51:42 +0530

    SA-forget pass added to login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('loginPage',{static:false}) loginPage: ElementRef;
+  @ViewChild('forgetPassPage',{static:false}) forgetPassPage: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  forgotPassword() {
+    this.loginPage.nativeElement.hidden =  !this.loginPage.nativeElement.hidden;
+    this.forgetPassPage.nativeElement.hidden =  !this.forgetPassPage.nativeElement.hidden;
   }
 
 }
