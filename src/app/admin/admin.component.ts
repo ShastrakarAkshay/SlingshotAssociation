971e7d0eb7070bb6130eedf54c9194df8db1d27f
Shastrakar <ashastra@bmc.com>
2020-04-19 15:58:23 +0530

    SA-badge count removed from admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -10,25 +10,10 @@ import { UserConfig } from '../shared/interfaces/slingshot.interface';
 export class AdminComponent implements OnInit {
 
   private userID: string;
-  private enquiryCount: number = 0;
-  private eventCount: number = 0;
-  private refreeCount: number = 0;
-
+ 
   constructor(private _service: SlingshotService) {
     this.userID = localStorage.getItem('user-id');
   }
 
-  ngOnInit() {
-    this._service.getEnquiries().subscribe(data => {
-      this.enquiryCount = data.length;
-    });
-
-    this._service.getAllEvents().subscribe(data => {
-      this.eventCount = data.length;
-    })
-
-    this._service.getAllRefrees().subscribe(data => {
-      this.refreeCount = data.length;
-    })
-  }
+  ngOnInit() {}
 }
