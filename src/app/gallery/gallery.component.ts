4889136eb2f7b2b1c0f858a5e5a5ec2aeabc1fa7
Shastrakar <ashastra@bmc.com>
2020-07-25 16:32:06 +0530

    SA-gallery upadated

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  private gallery: any[] = [];
+  showSpinner: boolean = false;
+  imageList: any[] = [[], [], []];
+
+  constructor(private afStorage: AngularFireStorage, private _spinner: NgxSpinnerService) { }
 
   ngOnInit() {
+    this.getAllImages();
+  }
+
+  getAllImages() {
+    this._showSpinner();
+    this.afStorage.storage.ref('Gallery/').listAll().then((result) => {
+      const chunk = (result.items.length / 3) + 1;
+      this.gallery = new Array(Math.ceil(result.items.length / chunk)).fill(0).map(_ => result.items.splice(0, chunk));
+      if (this.gallery.length > 0)
+        this.sliceImageArray();
+        this._hideSpinner();
+    });
+  }
+
+  sliceImageArray() {
+    this.gallery[0].forEach(item => {
+      const ref = this.afStorage.storage.ref().child('Gallery/' + item.name);
+      ref.getDownloadURL().then(url => { this.imageList[0].push({ documentURL: url }) });
+    })
+
+    this.gallery[1].forEach(item => {
+      const ref = this.afStorage.storage.ref().child('Gallery/' + item.name);
+      ref.getDownloadURL().then(url => { this.imageList[1].push({ documentURL: url }) });
+    })
+
+    this.gallery[2].forEach(item => {
+      const ref = this.afStorage.storage.ref().child('Gallery/' + item.name);
+      ref.getDownloadURL().then(url => { this.imageList[2].push({ documentURL: url }) });
+    })
+  }
+
+  private _showSpinner() {
+    this.showSpinner = true;
+    this._spinner.show();
+  }
+
+  private _hideSpinner() {
+    this._spinner.hide();
+    this.showSpinner = false;
   }
 
 }
